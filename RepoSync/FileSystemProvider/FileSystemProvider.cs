736f4067753d8a6ab24f61d63970fd5315e580eb
marosvolgyiz <zoltan.marosvolgyi@sensenet.com>
2017-04-21 14:50:20 +0200

    Change providers namespaces

--- RepoSync/FileSystemProvider/FileSystemProvider.cs
+++ RepoSync/FileSystemProvider/FileSystemProvider.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileSystemProvider
+namespace RepoSync.Providers.FileSystemProvider
 {
     public class FileSystemProvider : IRepoSyncProvider
     {
--- RepoSync/MemoryProvider/MemoryProvider.cs
+++ RepoSync/MemoryProvider/MemoryProvider.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MemoryProvider
+namespace RepoSync.Providers.MemoryProvider
 {
     public class MemoryProvider : IRepoSyncProvider
     {
