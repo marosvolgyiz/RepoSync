17877629639fed548fb87f22bc150116a93e6768
Dániel Blaskó <blaskod@sensenet.com>
2017-08-15 10:17:22 +0200

    WPF app UI

---
+++ RepoSync/ReposSyncWPFApp/Code/Settings/RepoSyncDrive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoSync.WPFApp.Code.Settings
+{
+    public class RepoSyncDrive
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Provider { get; set; }
+        public string FA_Icon { get; set; }
+        public List<KeyValuePair<string,string>> ProviderArguments { get; set; }
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
--- RepoSync/ReposSyncWPFApp/Settings.xaml.cs
+++ RepoSync/ReposSyncWPFApp/Settings.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ReposSyncWPFApp
+namespace RepoSync.WPFApp
 {
     /// <summary>
     /// Interaction logic for Settings.xaml
